356f6b5ae6f31dbce2e1e8b4a474566e3c51a705
unknown <caseychen3605@gmail.com>
2021-03-20 15:08:49 -0700

    Add and change map marker style

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -14,8 +14,15 @@ let map = L.map("mapid", {
 // The setView() method sets the view of the map with a geographical center, 
 //     where the first coordinate is the latitude and the second is longitude. Set the zoom level to "4" (0-18)
 
+//  Add a marker to the map for Los Angeles, California.
+L.circleMarker([34.0522, -118.2437], {
+  radius: 300,
+  color: "black",
+  fillColor: "#ffffa1"
+}).addTo(map);
+
 // Create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
